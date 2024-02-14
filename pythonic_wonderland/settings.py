26bdf7294c4558536600fb13f2f6ea8a77fe343a
Keenan <110526824+Keeks87@users.noreply.github.com>
2024-02-14 08:24:57 +0200

    Update settings to resolve admin css

--- pythonic_wonderland/settings.py
+++ pythonic_wonderland/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = []
 
 
 MEDIA_URL = "/media/"
