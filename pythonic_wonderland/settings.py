7e426b1745684a3dfe02ad9001ac6878178b87c7
Keenan <110526824+Keeks87@users.noreply.github.com>
2024-02-14 08:12:19 +0200

    Update settings

--- pythonic_wonderland/settings.py
+++ pythonic_wonderland/settings.py
@@ -15,9 +15,13 @@ import os
 import dj_database_url
 import environ
 from dotenv import load_dotenv
+import mimetypes
 
 # Load environment variables
 load_dotenv()
+
+# Add .css to mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
