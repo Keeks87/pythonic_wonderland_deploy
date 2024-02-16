99f971ffb288eb34d8d5fb6188d15198166a7723
Keenan <110526824+Keeks87@users.noreply.github.com>
2024-02-16 08:09:51 +0200

    Update urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,6 +6,5 @@ from . import views
 
 # URLs patterns
 urlpatterns = [
-    path('', views.landing, name='landing'),
-    path('portfolio/', views.portfolio, name='portfolio'),
+    path('', views.portfolio, name='portfolio'),
 ]
